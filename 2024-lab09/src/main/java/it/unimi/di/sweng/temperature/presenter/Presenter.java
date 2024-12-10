fd71175dd8c12967a4aea7c07234b676171a866a
mattia.isetta <mattia.isetta@studenti.unimi.it>
2024-12-10 18:20:03 +0100

    removed lab09

--- 2024-lab09/src/main/java/it/unimi/di/sweng/temperature/presenter/Presenter.java
+++
@@ -1,8 +0,0 @@
-package it.unimi.di.sweng.temperature.presenter;
-
-import it.unimi.di.sweng.temperature.Observer;
-import org.jetbrains.annotations.NotNull;
-
-public interface Presenter {
-  void action(@NotNull String text);
-}
--- 2024-lab09/src/main/java/it/unimi/di/sweng/temperature/presenter/ScaleStrategy.java
+++
@@ -1,7 +0,0 @@
-package it.unimi.di.sweng.temperature.presenter;
-
-public interface ScaleStrategy {
-  double convertFromCelsius(double temperature);
-
-  double convertToCelsius(double temperature);
-}
